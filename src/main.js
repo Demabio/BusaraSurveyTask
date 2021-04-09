bd8032221146c616cecd473fbae2ccfa9ace9aec
demabio <demabio91@gmail.com>
2021-04-10 01:51:01 +0300

    JD-changes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://104.248.0.49'
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/http://104.248.0.49'
 
 createApp(App).use(store).use(router, axios).mount('#app')
